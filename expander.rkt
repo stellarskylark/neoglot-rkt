31efb4e5e308bd27e563990b2f080374dc93f017
Andrew Hill <andrew@andrewkhill.com>
2020-04-28 20:21:38 -0500

    Get basic syllable-as-category-list working

--- expander.rkt
+++ expander.rkt
@@ -2,6 +2,9 @@
 (require (for-syntax syntax/parse))
 (require (for-syntax racket/base))
 
+(require racket/list)
+(require racket/string)
+
 (define-syntax (ng-mb stx)
   (syntax-parse stx
     [((~literal ng-mb) PARSE-TREE)
@@ -9,12 +12,13 @@
 (provide (rename-out [ng-mb #%module-begin]))
 
 (define cat-hash (make-hash))
+(define syll-list empty)
 
 (define-syntax (arg stx)
   (syntax-parse stx
     [((~literal arg) THE-ARG)
      #'THE-ARG]
-    [((~literal arg) "$" THE-ARG)
+    [((~literal arg) #f THE-ARG)
      #'(hash-ref cat-hash THE-ARG)]
     ))
 (provide arg)
@@ -29,14 +33,35 @@
      #'(and ARG1 ARG2)]
     [((~literal infix-op-exp) ARG1 "|" ARG2)
      #'(or ARG1 ARG2)]
+    [((~literal infix-op-exp) ARG1 "+" ARG2)
+     #'(flatten (list ARG1 ARG2))]
+    [((~literal infix-op-exp) ARG1 "-" ARG2)
+     #'(filter
+         (lambda (x)
+           (not (member x ARG2)))
+         ARG1)]
+;;    [((~literal infix-op-exp) ARG1 "@" ARG2)]
     ))
 (provide infix-op-exp)
 
 (define-syntax (cat-block stx)
   (syntax-parse stx
     [((~literal cat-block) CAT-NAME BLOCK)
-     #'(hash-set! cat-hash CAT-NAME (car BLOCK))]))
+     #'(hash-set! cat-hash CAT-NAME (first BLOCK))]))
 (provide cat-block)
+
+(define (add-syll s)
+  (set! syll-list (cons s syll-list)))
+
+(define-syntax (syll-block stx)
+  (syntax-parse stx
+    [((~literal syll-block) BLOCK)
+     #'(add-syll (lambda ()
+                   (flatten
+                     (map (lambda (ph)
+                            (first (shuffle ph)))
+                          (first BLOCK)))))]))
+(provide syll-block)
 
 ; Annoying that these are the same, but even if they
 ; expand the same, they serve different syntactic functions
@@ -50,12 +75,18 @@
      #'(list ARG ...)]))
 (provide expr wrap)
 
+(define (gen-syll)
+  ((first (shuffle syll-list))))
+
 (define-syntax (ng-program stx)
   (syntax-parse stx
     [((~literal ng-program) BLOCK ...)
      #'(begin
-          ((lambda x BLOCK ...))
-          (display cat-hash))]))
+          BLOCK ...
+          (displayln
+            (string-join
+              (flatten (list (gen-syll) (gen-syll)))
+              "")))]))
 (provide ng-program)
 
 (provide #%top #%app #%datum #%top-interaction)
